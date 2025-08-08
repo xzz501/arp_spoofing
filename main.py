9a4fca9b9fc7714a2c968b802a4f05c7ceda9fb7
xzz501 <xzz501@github.com>
2025-08-09 07:54:38 +1100

    get the main arguments by optparse

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 
 from scipy.all import *
+import optparse
+def get_arguments():
+    par=optparse.OptionParser()
+    par.add_option("-t","--taget",dest="target_ip" ,help="target IP")
+    par.add_option("-r", "--router", dest="getway_ip", help="getway  IP")
+    opt=par.parse_args()
+    if not opt.target_ip:
+        par.error("[-] plese enter ip of target")
+    if not opt.getwat_ip:
+        par.error("[-] please enter ip of getway")
+    return opt
+
